5315b28bcd798d87e8410c173c65b0d0a991304f
Pawel Boguslawski <bogusweb@users.noreply.github.com>
2026-09-21 01:40:52 +0200

    Split 512px pack PNGs into concatenable base64 parts

---
+++ lib/brand/pack-icon-bytes/icon-512-png.ts
@@ -0,0 +1,5 @@
+import { ICON_512_PNG_A } from "./icon_512_png-a";
+import { ICON_512_PNG_B } from "./icon_512_png-b";
+
+/** Exact bytes of marketing pack `favicons/icon-512.png`. */
+export const ICON_512_PNG = ICON_512_PNG_A + ICON_512_PNG_B;
